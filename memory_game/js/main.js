a43c5137dba2099ee7b1bbd026ae9f14b2149f1b
Valeria Bocanegra <Campanita@ivans-mbp.home>
2019-02-24 13:47:06 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,31 @@
 
-console.log("Up and running!");
+var cards = ["Queen", "Queen", "King", "King"];
 
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cardsInPlay = [];
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+var cardOne = cards [0];
+var cardTwo = cards [2];
+
+
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
+
+console.log("Use flipped queen");
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2)
+{
+	if (cardsInPlay[0] === cardsInPlay[1])
+	{
+		alert ("You found a match!.")
+	}
+	else 
+	{
+		alert ("Sorry, try again.")
+	}
+}
+
+
+
+
 
